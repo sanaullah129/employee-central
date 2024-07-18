b8dff151b101d5f7b9a702f647a8de46a3fdcfb6
sanaullah129 <asanaullah129@gmail.com>
2024-07-18 16:38:16 +0530

    Login & Register Routes Completed

--- backend/server/controllers/UserController.ts
+++ backend/server/controllers/UserController.ts
@@ -1,18 +1,110 @@
 import { Request, Response } from "express";
 import { CreateUser, IsUserAvailableCheck } from "../services/UserService";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerAdmin = async (req: Request, res: Response) => {
-    const { username, password, companyName, firstName, lastName, emailId, phoneNumber } = req.body;
+  try {
+    const {
+      username,
+      password,
+      companyName,
+      firstName,
+      lastName,
+      emailId,
+      phoneNumber,
+      isAdmin,
+    } = req.body;
 
-    if (!username || !password || !companyName || !firstName || !lastName || !emailId || !phoneNumber) {
-        return res.status(400).json({ statusId: 2, status: "Please fill all the details" });
+    if (
+      !username ||
+      !password ||
+      !companyName ||
+      !firstName ||
+      !lastName ||
+      !emailId ||
+      !phoneNumber
+    ) {
+      return res
+        .status(400)
+        .json({ statusId: 2, status: "Please fill all the details" });
     }
 
-    const IsUserAvailable: boolean = await IsUserAvailableCheck(username, emailId);
-
+    const IsUserAvailable = await IsUserAvailableCheck(username);
     if (IsUserAvailable) {
-        return res.status(400).json({ statusId: 0, status: "User already exists" });
-    };
+      return res
+        .status(400)
+        .json({ statusId: 3, status: "User already exists" });
+    }
 
-    const createUser = await CreateUser({ username, password, companyName, firstName, lastName, emailId, phoneNumber });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createUser = await CreateUser({
+      username,
+      password: hashedPassword,
+      companyName,
+      firstName,
+      lastName,
+      emailId,
+      phoneNumber,
+      isAdmin,
+    });
+    if (createUser) {
+      return res
+        .status(200)
+        .json({ statusId: 1, status: "User Created Successfully" });
+    } else {
+      return res.status(500).json({
+        statusId: 0,
+        status: "Some error occured while creating user",
+      });
+    }
+  } catch (error: any) {
+    console.log("Error in register admin: " + error);
+    res.status(500).json({ Error: "Internal Server Error" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Please Enter all the Details" });
+    }
+
+    const existingUser = await IsUserAvailableCheck(username);
+
+    if(!existingUser){
+        return res.status(404).json({statusId: 1, status: "No User with this username found"});
+    }
+    let isPasswordCorrect: boolean = false;
+
+    if (existingUser) {
+      isPasswordCorrect = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+    }
+
+    if(existingUser && isPasswordCorrect){
+        const jwtSecret = process.env.JWT_TOKEN as string;
+
+        if (jwtSecret) {
+            //jwt token
+            const token: string = jwt.sign(
+              {
+                username: existingUser.username,
+                password: existingUser.password,
+                id: existingUser.id,
+              },
+              jwtSecret,
+              { expiresIn: "1d" }
+            );    
+            res.json({ Bearer: token });
+          }
+
+    }
+    else{
+        res.status(401).json({statusId: 2, status: "Invalid Username and Password Combination"});
+    }
+
 };
--- backend/server/services/UserService.ts
+++ backend/server/services/UserService.ts
@@ -1,30 +1,40 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const IsUserAvailableCheck = async (username: string, emailId: string): Promise<boolean> => {
-  const isUserAvailable = await prisma.admin.findFirst({
-    where: {
-      AND: [
-        { username: username },
-        { emailId: emailId },
-      ],
-    },
+export const IsUserAvailableCheck = async (
+  username: string
+) => {
+  const isUserAvailable = await prisma.userDetails.findFirst({
+    where: { username: username },
   });
 
-  return isUserAvailable !== null;
+  return isUserAvailable;
 };
 
-export const CreateUser = async (data: {username: string; password: string; companyName: string; firstName: string; lastName: string; emailId: string; phoneNumber: number;}) => {
-    const newUser = await prisma.admin.create({
-        data: {
-          username: data.username,
-          password: data.password,
-          companyName: data.companyName,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          emailId: data.emailId,
-          phoneNumber: data.phoneNumber,
-        },
-      });
-} 
+export const CreateUser = async (data: {
+  username: string;
+  password: string;
+  companyName: string;
+  firstName: string;
+  lastName: string;
+  emailId: string;
+  phoneNumber: number;
+  isAdmin: boolean;
+}) => {
+  console.log(data.password);
+
+  const newUser = await prisma.userDetails.create({
+    data: {
+      username: data.username,
+      password: data.password,
+      companyName: data.companyName,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      emailId: data.emailId,
+      phoneNumber: data.phoneNumber,
+      isAdmin: data.isAdmin,
+    },
+  });
+  return newUser;
+};

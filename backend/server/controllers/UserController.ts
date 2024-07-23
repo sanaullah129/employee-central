b929923742d89c7faae36f4d65dea81285f389d6
sanaullah129 <asanaullah129@gmail.com>
2024-07-23 14:07:20 +0530

    shcema updated for profile

--- backend/server/controllers/UserController.ts
+++ backend/server/controllers/UserController.ts
@@ -66,45 +66,51 @@ export const registerAdmin = async (req: Request, res: Response) => {
 };
 
 export const loginUser = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return res.status(400).json({ message: "Please Enter all the Details" });
-    }
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please Enter all the Details" });
+  }
 
-    const existingUser = await IsUserAvailableCheck(username);
+  const existingUser = await IsUserAvailableCheck(username);
 
-    if(!existingUser){
-        return res.status(404).json({statusId: 1, status: "No User with this username found"});
-    }
-    let isPasswordCorrect: boolean = false;
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ statusId: 1, status: "No User with this username found" });
+  }
+  let isPasswordCorrect: boolean = false;
 
-    if (existingUser) {
-      isPasswordCorrect = await bcrypt.compare(
-        password,
-        existingUser.password
+  if (existingUser) {
+    isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+  }
+
+  if (existingUser && isPasswordCorrect) {
+    const jwtSecret = process.env.JWT_TOKEN as string;
+
+    if (jwtSecret) {
+      //jwt token
+      const token: string = jwt.sign(
+        {
+          username: existingUser.username,
+          password: existingUser.password,
+          id: existingUser.id,
+        },
+        jwtSecret,
+        { expiresIn: "1d" }
       );
+      res.json({ Bearer: token });
     }
-
-    if(existingUser && isPasswordCorrect){
-        const jwtSecret = process.env.JWT_TOKEN as string;
-
-        if (jwtSecret) {
-            //jwt token
-            const token: string = jwt.sign(
-              {
-                username: existingUser.username,
-                password: existingUser.password,
-                id: existingUser.id,
-              },
-              jwtSecret,
-              { expiresIn: "1d" }
-            );    
-            res.json({ Bearer: token });
-          }
-
-    }
-    else{
-        res.status(401).json({statusId: 2, status: "Invalid Username and Password Combination"});
-    }
-
+  } else {
+    res
+      .status(401)
+      .json({
+        statusId: 2,
+        status: "Invalid Username and Password Combination",
+      });
+  }
 };
+
+export const addEmployee = async (req: Request, res: Response) => {
+  console.log(req.body);
+  // const {  } = req.body;
+}
